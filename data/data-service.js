474198fb12a8ee27942c667c009a512b64b4b7a4
Keith <kcao13@myseneca.ca>
2022-09-27 18:49:57 -0400

    added initialize in data-service.js

--- data/data-service.js
+++ data/data-service.js
@@ -1,1 +1,23 @@
+students = [];
+programs = [];
 
+module.exports.intialize = function() {
+    return new Promise((resolve,reject)=> {
+        fs.readFile('./students.json', 'utf8', (err, data) => {
+            if (err){
+                reject("Cannot read file"); 
+                return;
+            }
+            console.log(data);
+            students = JSON.parse(data);
+       }).then(fs.readFile('./programs.json', 'utf8', (err, data) => {
+        if (err){
+            reject("Cannot read file"); 
+            return;
+        }
+        console.log(data);
+        programs = JSON.parse(data);
+    }));
+       resolve();
+    }); 
+}
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('data/data-service.js')
+require('./data/data-service.js')
 express = require('express');
 path = require('path');
 app = express();
@@ -17,5 +17,22 @@ app.get("/", (req,res) => {
 app.get("/about", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
+
+app.get("/students", (req,res) => {
+    res.send("retrieve students");
+});
+
+app.get("/intlstudents", (req,res) => {
+    res.send("retrieve international students");
+});
+
+app.get("/programs", (req,res) => {
+    res.send("retrieve programs");
+});
+
+app.use((req,res) => {
+    res.status(404).send(<h1>404 <br>NOT FOUND</br></h1>);
+})
+
 
 app.listen(HTTP_PORT, onServerStart);
