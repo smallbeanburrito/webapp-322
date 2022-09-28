ee8c960e3b1e9b8fc97943fccb0f8dd450040ed0
Keith <kcao13@myseneca.ca>
2022-09-28 00:34:15 -0400

    added initialize  and get functions

--- data/data-service.js
+++ data/data-service.js
@@ -1,23 +1,59 @@
+const fs = require('fs');
 students = [];
 programs = [];
 
-module.exports.intialize = function() {
+module.exports.initialize = function() {
     return new Promise((resolve,reject)=> {
-        fs.readFile('./students.json', 'utf8', (err, data) => {
-            if (err){
-                reject("Cannot read file"); 
-                return;
+        fs.readFile('./data/students.json', 'utf8', (err, data) => {
+            if (err) {
+                reject("Could not load students");
             }
-            console.log(data);
-            students = JSON.parse(data);
-       }).then(fs.readFile('./programs.json', 'utf8', (err, data) => {
-        if (err){
-            reject("Cannot read file"); 
-            return;
-        }
-        console.log(data);
-        programs = JSON.parse(data);
-    }));
-       resolve();
+            else{
+                students = JSON.parse(data);
+                fs.readFile('./data/programs.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject("Could not load programs");
+                    }
+                    else {
+                        programs = JSON.parse(data);
+                        resolve();
+                    }
+                })
+            }
+       })
     }); 
+}
+
+module.exports.getAllStudents = function() {
+    return new Promise((resolve, reject) => {
+        if(length(students) == 0) {
+            reject("No students found"); return;
+        }
+        else {
+            resolve(students);
+        }
+    });
+}
+
+module.exports.getInternationalStudents = function() {
+    return new Promise((resolve, reject) => {
+        if(length(students) == 0) {
+            reject("No students found"); return;
+        }
+        else {
+            internationalStudents = students.filter((e) => e.isInternationalStudent == True);
+            resolve(students.filter(internationalStudents));
+        }
+    });
+}
+
+module.exports.getPrograms = function() {
+    return new Promise((resolve, reject) => {
+        if(length(programss) == 0) {
+            reject("No programs found"); return;
+        }
+        else {
+            resolve(programs);
+        }
+    });
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('./data/data-service.js')
+dataService = require('./data/data-service.js')
 express = require('express');
-path = require('path');
 app = express();
+path = require('path');
 app.use(express.static('public'));
 
 HTTP_PORT = process.env.PORT || 8080;
@@ -31,8 +31,11 @@ app.get("/programs", (req,res) => {
 });
 
 app.use((req,res) => {
-    res.status(404).send(<h1>404 <br>NOT FOUND</br></h1>);
+    res.status(404).send(`<h1>404 <br>NOT FOUND</br></h1>`);
 })
 
+dataService.initialize()
+.then(() => app.listen(HTTP_PORT, onServerStart))
+.catch((msg) => console.log(msg));
 
-app.listen(HTTP_PORT, onServerStart);
+
