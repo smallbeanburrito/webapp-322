dcdebf13449ac6a11e6f4dcc9801877831cf9fda
Keith <kcao13@myseneca.ca>
2022-10-13 00:30:13 -0400

    assignment 3

---
+++ data-service.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+students = [];
+programs = [];
+
+module.exports.initialize = function() {
+    return new Promise((resolve,reject)=> {
+        fs.readFile('./students.json', 'utf8', (err, data) => {
+            if (err) {
+                reject("Could not load students");
+            }
+            else{
+                students = JSON.parse(data);
+                fs.readFile('./programs.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject("Could not load programs");
+                    }
+                    else {
+                        programs = JSON.parse(data);
+                        resolve();
+                    }
+                })
+            }
+       })
+    }); 
+}
+
+module.exports.getAllStudents = function() {
+    return new Promise((resolve, reject) => {
+        if(programs.length == 0) {
+            reject("No students found"); return;
+        }
+        else {
+            resolve(students);
+        }
+    });
+}
+
+module.exports.getInternationalStudents = function() {
+    return new Promise((resolve, reject) => {
+        if(students.length == 0) {
+            reject("No students found"); return;
+        }
+        else {
+            internationalStudents = students.filter((e) => e.isInternationalStudent == true);
+            resolve(internationalStudents);
+        }
+    });
+}
+
+module.exports.getPrograms = function() {
+    return new Promise((resolve, reject) => {
+        if(programs.length == 0) {
+            reject("No programs found"); return;
+        }
+        else {
+            resolve(programs);
+        }
+    });
+}
+
+module.exports.addStudent = function(studentData) {
+    return new Promise((resolve, reject) => {
+        if (studentData.isInternationalStudent == undefined) {
+            studentData.isInternationalStudent = false;
+        }
+        else {
+            studentData.isInternationalStudent = true;
+        }
+        function max() {
+            var max = 0;
+            students.forEach(student => {
+                if(Number(student.studentID) > max) {
+                    max = Number(student.studentID);
+                }
+            });
+            return max+1;
+        }
+        const newStudentNumber = max();
+        studentData.studentID = newStudentNumber.toString();
+        students.push(studentData);
+        resolve();
+    });
+}
+
+module.exports.getStudentsByStatus = function(status){
+    return new Promise((resolve, reject) =>{
+        studentStatus = students.filter((student) => student.status.localeCompare(status) == 0)
+        if(studentStatus.length == 0) {
+            reject("No students with status: " + status); return;
+        }
+        resolve(studentStatus);
+    });
+}
+
+module.exports.getStudentsByProgramCode = function(programCode){
+    return new Promise((resolve, reject) =>{
+        studentCode = students.filter((student) => student.program.localeCompare(programCode) == 0)
+        if(studentCode.length == 0) {
+            reject("No students with code: " + programCode); return;
+        }
+        resolve(studentCode);
+    });
+}
+
+module.exports.getStudentsByExpectedCredential = function(credential){
+    return new Promise((resolve, reject) =>{
+        studentCred = students.filter((student) => student.expectedCredential && student.expectedCredential.localeCompare(credential) == 0);
+        if(studentCred.length == 0) {
+            reject("No students with credentials: " + credential); return;
+        }
+        resolve(studentCred);
+    });
+}
+
+module.exports.getStudentById = function(sid) {
+    return new Promise((resolve, reject) => {
+        studentId = students.find((student) => student.studentID == sid);
+        if(studentId) {
+            resolve(studentId); return;
+        }
+        reject("No student with ID: " + sid);
+    });
+}
--- server.js
+++ server.js
@@ -3,24 +3,36 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Keith Cao Student ID: 1443332211 Date: Sept. 28, 2022
+*  Name: Keith Cao Student ID: 1443332211 Date: Oct. 13, 2022
 *
 *  Online (Cyclic) Link: https://rich-jade-centipede-slip.cyclic.app/
 *
 ********************************************************************************/ 
 
-
-dataService = require('./data/data-service.js')
+const fs = require('fs');
+dataService = require('./data-service.js')
 express = require('express');
 app = express();
 path = require('path');
 app.use(express.static('public'));
+multer = require('multer')
 
 HTTP_PORT = process.env.PORT || 8080;
 
 function onServerStart() {
     console.log("Express http server listening on " + HTTP_PORT);
 }
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+      }
+});
+
+const upload = multer({storage: storage});
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"));
@@ -30,22 +42,71 @@ app.get("/about", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
+app.get("/students/add", (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/addStudent.html"));
+});
+
+app.get("/images/add", (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
 app.get("/students", (req,res) => {
-    dataService.getAllStudents()
-    .then((students) => res.json(students))
-    .catch((msg) => console.log(msg));
+    if(req.query.status) {
+        dataService.getStudentsByStatus(req.query.status)
+        .then((students) => res.json(students))
+        .catch((msg) => res.send(msg));
+    }
+    else if(req.query.program) {
+        dataService.getStudentsByProgramCode(req.query.program)
+        .then((students) => res.json(students))
+        .catch((msg) => res.send(msg));
+    }
+    else if (req.query.credential) {
+        dataService.getStudentsByExpectedCredential(req.query.credential)
+        .then((students) => res.json(students))
+        .catch((msg) => res.send(msg));
+    }
+    else {
+        dataService.getAllStudents()
+        .then((students) => res.json(students))
+        .catch((msg) => res.send(msg));
+    }
+});
+
+app.get("/students/:value", (req,res) => {
+    dataService.getStudentById(req.params.value)
+    .then((student) => res.json(student))
+    .catch((msg) => res.send(msg));
 });
 
 app.get("/intlstudents", (req,res) => {
     dataService.getInternationalStudents()
     .then((students) => res.json(students))
-    .catch((msg) => console.log(msg));
+    .catch((msg) => res.send(msg));
 });
 
 app.get("/programs", (req,res) => {
     dataService.getPrograms()
     .then((programs) => res.json(programs))
-    .catch((msg) => console.log(msg));
+    .catch((msg) => res.send(msg));
+});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+});
+
+app.get("/images", (req,res) => {
+    fs.readdir('./public/images/uploaded', (err, data) => {
+        res.json({
+            images: data
+        });
+    });
+});
+
+app.post("/students/add", (req, res) => {
+    dataService.addStudent(req.body)
+    .then(() => res.redirect("/students"))
+    .catch(() => console.log("Could not add student"));
 });
 
 app.use((req,res) => {
